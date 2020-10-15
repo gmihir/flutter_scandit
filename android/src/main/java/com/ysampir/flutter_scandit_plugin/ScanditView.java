81c95c427721feef494698e2164dc614a1be7156
Mihir Gupta <m08.in.us@gmail.com>
2020-10-15 15:01:05 -0700

    fixed import

--- android/src/main/java/com/ysampir/flutter_scandit_plugin/ScanditView.java
+++ android/src/main/java/com/ysampir/flutter_scandit_plugin/ScanditView.java
@@ -11,8 +11,7 @@ import com.scandit.datacapture.barcode.capture.BarcodeCaptureSession;
 import com.scandit.datacapture.barcode.capture.BarcodeCaptureSettings;
 import com.scandit.datacapture.barcode.data.Barcode;
 import com.scandit.datacapture.barcode.data.Symbology;
-import com.scandit.datacapture.barcode.data.SymbologySettings;
-
+import com.scandit.datacapture.barcode.capture.SymbologySettings;
 import com.scandit.datacapture.core.capture.DataCaptureContext;
 import com.scandit.datacapture.core.data.FrameData;
 import com.scandit.datacapture.core.source.Camera;
